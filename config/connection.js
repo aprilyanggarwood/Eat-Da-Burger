5eddd2e8008573546c492b5e29ed8e3563c3b228
aprilyanggarwood <yangyyj822@hotmail.com>
2020-12-12 10:02:15 -0800

    make it better

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: process.env.SQLPW,
-    database: "burgers_db",
+    user: "lynlfnryjz8d6cfb",
+    password: u8r3ibdadbmyvnmu,
+    database: "kb94lkxt0vfqtspc",
   });
 }
 
